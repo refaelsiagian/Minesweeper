7642353cee6b4681e73e7ebb867ba6d3aa6b90cf
Refael Siagian <juarisiagian21106@gmail.com>
2025-05-11 01:09:58 +0700

    Move, board, mines

--- board.py
+++ board.py
@@ -8,7 +8,6 @@ NUM_MINES = 10
 
 # Inisialisasi papan kosong
 board = [[0 for _ in range(COLS)] for _ in range(ROWS)]
-# Papan penutup (False = tertutup, True = terbuka)
 revealed = [[False for _ in range(COLS)] for _ in range(ROWS)]
 
 # Sebar bom secara acak
@@ -20,7 +19,7 @@ while len(mines) < NUM_MINES:
 
 # Tandai bom di papan
 for r, c in mines:
-    board[r][c] = -1  # Gunakan -1 untuk bom
+    board[r][c] = -1
 
 # Hitung angka di sekitar bom
 for r, c in mines:
@@ -30,21 +29,31 @@ for r, c in mines:
             if 0 <= nr < ROWS and 0 <= nc < COLS and board[nr][nc] != -1:
                 board[nr][nc] += 1
 
+# Posisi terakhir yang dipilih
+last_move = (-1, -1)
+
 def print_board():
     print("\nPapan:")
     for i in range(ROWS):
         row_str = ''
         for j in range(COLS):
+            cell_str = ''
             if revealed[i][j]:
                 if board[i][j] == -1:
-                    row_str += '* '
+                    cell_str = '*'
+                elif board[i][j] == 0:
+                    cell_str = '.'
                 else:
-                    row_str += f'{board[i][j]} '
+                    cell_str = f'{board[i][j]}'
             else:
-                row_str += 'X '
+                cell_str = 'X'
+
+            if (i, j) == last_move:
+                row_str += f'[{cell_str}] '
+            else:
+                row_str += f' {cell_str}  '
         print(row_str)
 
-# Fungsi membuka area kosong pakai BFS
 def reveal_area(r, c):
     queue = deque()
     queue.append((r, c))
@@ -64,17 +73,21 @@ def reveal_area(r, c):
                     if 0 <= nr < ROWS and 0 <= nc < COLS and (nr, nc) not in visited:
                         queue.append((nr, nc))
 
+def reveal_all_mines():
+    for r, c in mines:
+        revealed[r][c] = True
 
-# Main loop
 while True:
     print_board()
     try:
         inp = input("Masukkan koordinat (baris kolom, mulai dari 1), misal 3 4: ")
         r, c = map(int, inp.strip().split())
-        r -= 1  # Sesuaikan ke indeks 0
+        r -= 1
         c -= 1
         if 0 <= r < ROWS and 0 <= c < COLS:
+            last_move = (r, c)
             if board[r][c] == -1:
+                reveal_all_mines()
                 revealed[r][c] = True
                 print_board()
                 print("BOOM! Kena bom. Game over!")
